f03bfc0f1cb04bebf5c44878793661374eb6cf23
khalidfarig1 <khalidfarig1@users.noreply.github.com>
2021-07-07 12:00:00 -0400

    Initial commit: Add Python Hello World program

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Simple Hello World program in Python
+Created: July 7, 2021
+"""
+
+def main():
+    """Main function to print Hello World message"""
+    print("Hello, World!")
+    print("Welcome to Python programming!")
+
+if __name__ == "__main__":
+    main()
